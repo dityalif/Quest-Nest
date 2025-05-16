0cff18a3295da9ad9d5c615cd7a84fb88d7627f2
Alfonsus Tanara <alfonsustanara@gmail.com>
2025-05-16 17:07:25 +0700

    update query

--- dump.sql
+++ dump.sql
@@ -1,5 +1,8 @@
+-- Aktifkan ekstensi UUID
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
@@ -9,7 +12,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE challenges (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title VARCHAR(100) NOT NULL,
     description TEXT,
     category VARCHAR(50),
@@ -17,32 +20,56 @@ CREATE TABLE challenges (
     points INTEGER DEFAULT 0,
     type VARCHAR(10) CHECK (type IN ('individu', 'tim')),
     deadline DATE,
-    creator_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+    creator_id UUID REFERENCES users(id) ON DELETE SET NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 
 CREATE TABLE teams (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT,
-    creator_id INTEGER REFERENCES users(id),
+    creator_id UUID REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 CREATE TABLE badges (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     description TEXT,
     condition TEXT
 );
 
-
 CREATE TABLE leaderboards (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     type VARCHAR(10) CHECK (type IN ('user', 'team')),
-    reference_id INTEGER, 
+    reference_id UUID, 
     xp INTEGER DEFAULT 0,
-    period VARCHAR(20) 
+    period VARCHAR(20)
+);
+
+
+CREATE TABLE team_members (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    team_id UUID REFERENCES teams(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    role VARCHAR(20) DEFAULT 'member', -- contoh: 'member', 'admin'
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(team_id, user_id)
+);
+
+CREATE TABLE team_challenges (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    team_id UUID REFERENCES teams(id) ON DELETE CASCADE,
+    challenge_id UUID REFERENCES challenges(id) ON DELETE CASCADE,
+    assigned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE challenge_participants (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    challenge_id UUID REFERENCES challenges(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    team_id UUID REFERENCES teams(id) ON DELETE CASCADE,
+    status VARCHAR(20) DEFAULT 'ongoing',
+    completed_at TIMESTAMP,
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
