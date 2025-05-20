91ea2a8d3262fb6899475f1b9f12c6a463aa6e86
Raditya Alif <ditya818@gmail.com>
2025-05-20 23:07:50 +0700

    deploy frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://quest-nest-be.vercel.app',
 });
 
 instance.interceptors.request.use((config) => {
